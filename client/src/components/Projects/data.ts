3c90deff4e7f88a633f15b4e9121b1fb50ba8bf7
Binup-st <2077csit07@hcoe.edu.np>
2025-03-25 20:45:14 +0545

    few-changes

--- client/src/components/Projects/data.ts
+++ client/src/components/Projects/data.ts
@@ -1,16 +1,26 @@
 export const data = [
-    {
-        id: 1,
-        title: "GPT-3",
-        description: "GPT-3 is a frontend-only React website showcasing OpenAI’s GPT-3 capabilities through structured content. It highlights applications in chatbots, knowledgebases, and education with a minimalist design.",
-        thumbnail: "/assets/gpt3.png",
-        url: "binup-gpt3.netlify.app"
-    },
-    {
-        id: 2,
-        title: "PopcornPicks",
-        description: "PopcornPicks is a React-based movie recommendation website that simplifies movie discovery. It integrates the TMDB API for real-time data on trending films, ratings, and descriptions. With a clean UI and smooth navigation, it offers a seamless user experience.",
-        thumbnail: "/assets/popcornPicks.png",
-        url: "https://popcornpicks-1.onrender.com/"
-    },
-]
+  {
+    id: 1,
+    title: "GPT-3",
+    description:
+      "GPT-3 is a frontend-only React website showcasing OpenAI’s GPT-3 capabilities through structured content. It highlights applications in chatbots, knowledgebases, and education with a minimalist design.",
+    thumbnail: "/assets/gpt3.png",
+    url: "binup-gpt3.netlify.app",
+  },
+  {
+    id: 2,
+    title: "PopcornPicks",
+    description:
+      "PopcornPicks is a React-based movie recommendation website that simplifies movie discovery. It integrates the TMDB API for real-time data on trending films, ratings, and descriptions. With a clean UI and smooth navigation, it offers a seamless user experience.",
+    thumbnail: "/assets/popcornPicks.png",
+    url: "https://popcornpicks-1.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "PopcornPicks",
+    description:
+      "Colour is a product listing web application built using Next.js, designed to showcase and manage products efficiently. It offers a clean UI for browsing and interacting with product details. The project is deployed on Vercel for seamless accessibility.",
+    thumbnail: "/assets/colour.png",
+    url: "https://colour-deploy.vercel.app/",
+  },
+];
